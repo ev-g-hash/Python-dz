98d83bc75c73c4a6557e2e3ada35716deece4304
evgeh-hash <pihtulovevgeny@gmail.com>
2025-06-04 04:11:52 +0400

    домашние задания

---
+++ 4. dz/dz_20/dz_20.py
@@ -0,0 +1,28 @@
+from my_module_algo import my_bublle_sort
+print()
+
+from my_module_algo import my_quicksort
+print()
+
+from my_module_algo import my_heapify
+print()
+
+from my_module_algo import my_shell
+print()
+
+from my_module_algo import my_insertion_sort
+print()
+
+from my_module_algo import my_merge
+print()
+
+
+
+
+
+
+
+
+
+
+
